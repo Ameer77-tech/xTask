ac0c6207cc758e175c97d74327d21490d7011a2f
Ameer77-tech <ameershaik.cs@gmail.com>
2025-12-04 20:21:16 +0530

    fix: cors origin fix 3

--- server/src/index.js
+++ server/src/index.js
@@ -22,22 +22,28 @@ const PORT = process.env.PORT;
 const frontend = process.env.CLIENT_URL;
 
 // middlewares
-app.use(cookieParser());
-app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  frontend,
+  "https://x-task-rho.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [frontend],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-
-app.options(
-  /.*/,
-  cors({
-    origin: [frontend],
-    credentials: true,
-  })
-);
+app.use(cookieParser());
+app.use(express.json());
 
 app.use(helmet());
 
